9f24d5006d1d142a38a365a18fa607e4adca6d76
Dan Bullock <daniel.i.bullock@gmail.com>
2015-04-06 18:46:51 -0600

    begun setting up routing for projects & resume

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-angular.module("portfolio", [])
+angular.module("portfolio", ['ngRoute'])
+  .config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: "./views/main.html"
+		})
+			.when("/projects", {
+				templateUrl: "./views/projects.html"
+})
+			.when("/resume", {
+				templateUrl: "./views/resume.html"
+		})
+		
+			.otherwise ({
+				redirectTo: '/'
+		})
+})
   .controller("mainCtrl", ["$scope", function ($scope) {
-    $scope.test = "dan";
+    
   }]);
