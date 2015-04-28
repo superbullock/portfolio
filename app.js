47abe6529738b1f640aeb86ded5cfc6ece7ff246
DANIEL BULLOCK <DANIEL.I.BULLOCK@gmail.com>
2015-04-28 13:47:11 -0600

    "added in routes"

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-angular.module("portfolio", [])
-  .controller("mainCtrl", ["$scope", function ($scope) {
-    $scope.test = "dan";
-  }]);
+angular.module("portfolio", ['ngRoute', 'ngAnimate'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: "templates/home.html"
+      })
+      .when('/projects', {
+        templateUrl: "templates/projects.html"
+      })
+      .when('/resume', {
+        templateUrl: "templates/resume.html"
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  })
+  .controller("mainCtrl", ["$scope", function ($scope) {}]);
+
+NG ANIMATED dan sucks
